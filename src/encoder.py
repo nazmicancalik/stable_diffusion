dae4576bdc2cff0a586be83fa17a9d12b126c0b6
Nazmican Calik <nazmicancalik@gmail.com>
2024-01-28 01:31:43 +0100

    feat: add more residual blocks

--- src/encoder.py
+++ src/encoder.py
@@ -7,6 +7,21 @@ class VAE_Encoder(nn.Sequential):
 
     def __init__(self):
         super.__init__(
+            # (batch_size, channel, height, width) -> (batch_size, 128, height, width)
             nn.Conv2d(3, 128, kernel_size=3, padding=1),
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+
+            # (batch_size, 128, height, width) -> (batch_size, 128, height, width)
+            VAE_ResidualBlock(128, 128)
+        
+            # (batch_size, 128, height, width) -> (batch_size, 128, height, width)
+            VAE_ResidualBlock(128, 128)
+
+            # (batch_size, 128, height, width) -> (batch_size, 128, height/2, width/2)
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=0)
+
+            # (batch_size, 128, height/2, width/2) -> (batch_size, 256, height/2, width/2)
+            VAE_ResidualBlock(128, 256)
+
+            # (batch_size, 256, height/2, width/2) -> (batch_size, 256, height/2, width/2)
+            VAE_ResidualBlock(256, 256)
         )
