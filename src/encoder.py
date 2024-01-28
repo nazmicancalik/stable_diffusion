abb7886b0dd712ce591847becc7d57e38818fe31
Nazmican Calik <nazmicancalik@gmail.com>
2024-01-28 15:01:19 +0100

    feat: add last modules for encoder

--- src/encoder.py
+++ src/encoder.py
@@ -1,27 +1,48 @@
 import torch
 from torch import nn
-from torch.nn import functional as F 
+from torch.nn import functional as F
 from decoder import VAE_AttentionBlock, VAE_ResidualBlock
 
-class VAE_Encoder(nn.Sequential):
 
+class VAE_Encoder(nn.Sequential):
     def __init__(self):
         super.__init__(
-            # (batch_size, channel, height, width) -> (batch_size, 128, height, width)
+            # (b, channel, h, w) -> (b, 128, h, w)
             nn.Conv2d(3, 128, kernel_size=3, padding=1),
-
-            # (batch_size, 128, height, width) -> (batch_size, 128, height, width)
-            VAE_ResidualBlock(128, 128)
-        
-            # (batch_size, 128, height, width) -> (batch_size, 128, height, width)
-            VAE_ResidualBlock(128, 128)
-
-            # (batch_size, 128, height, width) -> (batch_size, 128, height/2, width/2)
-            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=0)
-
-            # (batch_size, 128, height/2, width/2) -> (batch_size, 256, height/2, width/2)
-            VAE_ResidualBlock(128, 256)
-
-            # (batch_size, 256, height/2, width/2) -> (batch_size, 256, height/2, width/2)
-            VAE_ResidualBlock(256, 256)
+            # (b, 128, h, w) -> (b, 128, h, w)
+            VAE_ResidualBlock(128, 128),
+            # (b, 128, h, w) -> (b, 128, h, w)
+            VAE_ResidualBlock(128, 128),
+            # (b, 128, h, w) -> (b, 128, h/2, w/2)
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=0),
+            # (b, 128, h/2, w/2) -> (b, 256, h/2, w/2)
+            VAE_ResidualBlock(128, 256),
+            # (b, 256, h/2, w/2) -> (b, 256, h/2, w/2)
+            VAE_ResidualBlock(256, 256),
+            # (b, 256, h/2, w/2) -> (b, 256, h/2, w/2)
+            VAE_ResidualBlock(256, 256),
+            # (b, 256, h/2, w/2) -> (b, 256, h/4, w/4)
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=0),
+            # (b, 256, h/4, w/4) -> (b, 512, h/4, w/4)
+            VAE_ResidualBlock(256, 512),
+            # (b, 512, h/4, w/4) -> (b, 256, h/4, w/4)
+            VAE_ResidualBlock(512, 512),
+            # (b, 512, h/4, w/4) -> (b, 256, h/8, w/8)
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=0),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            # (b, 512, h/8, w/8) -> (b, 512, h/8, w/8)
+            VAE_ResidualBlock(512, 512),
+            # (b, 512, h/8, w/8) -> (b, 512, h/8, w/8)
+            VAE_AttentionBlock(512),
+            # (b, 512, h/8, w/8) -> (b, 512, h/8, w/8)
+            VAE_ResidualBlock(512, 512),
+            # (b, 512, h/8, w/8) -> (b, 512, h/8, w/8)
+            nn.GroupNorm(32, 512),
+            # (b, 512, h/8, w/8) -> (b, 512, h/8, w/8)
+            nn.SiLU(),
+            # (b, 512, h/8, w/8) -> (b, 8, h/8, w/8)
+            nn.Conv2d(512, 8, kernel_size=3, padding=1),
+            # (b, 8, h/8, w/8) -> (b, 8, h/8, w/8)
+            nn.Conv2d(8, 8, kernel_size=1, padding=0),
         )
