de7e311bbb9418d1358875659a3022aad1dda59c
Nazmican Calik <nazmicancalik@gmail.com>
2024-01-28 15:19:43 +0100

    feat: residual block modules added

---
+++ src/decoder.py
@@ -0,0 +1,21 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from attention import SelfAttention
+
+
+class VAE_ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super.__init__()
+        self.groupnorm1 = nn.GroupNorm(32, in_channels)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+        self.groupnorm2 = nn.GroupNorm(32, out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(
+                in_channels, out_channels, kernel_size=1, padding=0
+            )
