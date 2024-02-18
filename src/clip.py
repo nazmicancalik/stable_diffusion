041d393ad3ae79c4556c127241772537266df319
Nazmican Calik <nazmicancalik@gmail.com>
2024-02-19 00:54:57 +0100

    clip embedding

--- src/clip.py
+++ src/clip.py
@@ -4,6 +4,22 @@ from torch.nn import functional as F
 from attention import SelfAttention
 
 
+class CLIPEmbedding(nn.Module):
+    def __init__(self, n_vocab: int, n_embd: int, n_tokens: int):
+        super.__init__()
+
+        self.token_embedding = nn.Embedding(n_vocab, n_embd)
+        self.position_embedding = nn.Parameter(torch.zeros(n_tokens, n_embd))
+
+    def forward(self, tokens):
+        # (bs, seq_len) -> (bs, seq_len, dim)
+        x = self.token_embedding(tokens)
+
+        x += self.position_embedding
+
+        return x
+
+
 class CLIP(nn.module):
     def __init__(self):
         self.embedding = CLIPEmbedding(
