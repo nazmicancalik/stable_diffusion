7ec2f8a860e8c6519ed732b857c8445e17ed057a
Nazmican Calik <nazmicancalik@gmail.com>
2024-02-21 23:46:14 +0100

    implement forward of diffusion model

--- src/diffusion.py
+++ src/diffusion.py
@@ -16,4 +16,13 @@ class Diffusion(nn.Module):
         # context = (bs, seq_len, dim)
         # time = (1, 320)
 
+        # (1,320) -> (1, 1280)
         time = self.time_embedding(time)
+
+        # (bs, 4, height/8, width/8) -> (bs, 320, height/8, width/8)
+        output = self.unet(latent, context, time)
+
+        # (bs, 320, height/8, width/8) -> (bs, 4, height/8, width/8) Go back to original size
+        output = self.final(output)
+
+        return output
